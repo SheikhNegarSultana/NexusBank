38b0300af3d4a958fe76f6fb61146b96b1558ec5
SheikhNegarSultana <sultanashashi64@gmail.com>
2025-07-28 05:27:22 +0600

    Modal.js Updated

--- modals.js
+++ modals.js
@@ -46,6 +46,30 @@ function modals(){
         event.preventDefault();
         closeWithdrawModal();
     });
+
+
+    // Transaction Modal Start
+    const transactionsModal = document.getElementById('transactionsModal');
+    const closeTransactionBtn = document.getElementById('closeTransactionBtn');
+
+    function openTransactionsModal() {
+    transactionsModal.classList.add('modal-open');}
+    
+    function closeTransactionsModal() {
+    transactionsModal.classList.remove('modal-open');}
+
+    document.querySelectorAll('a[href="#transactions"]').forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+        openTransactionsModal();
+    });
+});
+
+    closeTransactionBtn.addEventListener('click', function(event) {
+        event.preventDefault();
+        closeTransactionsModal();
+    });
+
     
 }
 
